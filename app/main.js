03e17f2da4d0176ddd2aa98b947988ac4ff402dc
krisyang <krisyangjian@126.com>
2017-11-16 23:18:39 +0800

    asdasd

--- app/main.js
+++
@@ -1,9 +0,0 @@
-import { name } from './Greeter';
-console.log(name)
-import "./s.css"
-
-// window.setTimeout(function() {
-// 	import(/* webpackChunkName: "print" */ './print').then(function() {
-// 		console.log("print loaded...............................")
-// 	})
-// }, 5000)
