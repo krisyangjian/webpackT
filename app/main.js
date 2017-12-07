349255ffab467b6b5c01b9e48a4b5cb372c2ff91
krisyang <krisyangjian@126.com>
2017-12-07 21:07:47 +0800

    m0

--- app/main.js
+++ app/main.js
@@ -5,12 +5,7 @@
 
 // import "./a.css";
 import ab from "./icon.svg";
-var xyu  = null;
-var b_1  = "new";
-var vvv= 3;
-var master = "sssss";
-var other = "SAdasd"
-var master = "aaaaa";
+var master = "master";
 var ssss = require("./icon.svg");
 
 
