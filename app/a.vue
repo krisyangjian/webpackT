31350018b1341b66960d43a957ed78fd7ff759cb
krisyang <krisyangjian@126.com>
2017-12-21 09:38:58 +0800

    ss

---
+++ app/a.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="a-p">
+		Hello Vue
+  </div>
+</template>
+<script>
+import "./a.css"
+	export default {
+
+	}
+</script>
+<style>
+	.a-p {
+		font-size: 20px;
+	}
+</style>
---
+++ app/b.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="b-p">
+		Hello Vue B
+  </div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style>
+	.b-p {
+		font-size: 30px;
+	}
+</style>
